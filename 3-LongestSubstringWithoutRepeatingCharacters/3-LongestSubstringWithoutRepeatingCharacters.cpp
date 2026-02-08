f7c104983f3370a2f1babcefbae31d7782d81665
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-02-08 14:11:10 +0530

    File updated at 08/02/2026, 14:11:10

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,17 +1,17 @@
-// Last updated: 08/02/2026, 13:52:55
+// Last updated: 08/02/2026, 14:11:10
 1class Solution {
 2public:
 3    int lengthOfLongestSubstring(string s) {
-4        unordered_set<char> st;
+4        unordered_map<char,int> mp;
 5         int left=0,maxlen=0;
 6        for(int right=0;right<s.length();right++){
-7          while(st.find(s[right])!=st.end()){
-8            st.erase(s[left]);
-9            left++;
-10          }
-11          st.insert(s[right]);
-12          maxlen=max(maxlen,right-left+1);
-13        }
-14   return maxlen; }
+7          if(mp.find(s[right])!=mp.end()){
+8            left = max(left, mp[s[right]]+1);
+9          }
+10          mp[s[right]]=right;
+11          maxlen=max(maxlen,right-left+1);
+12        }
+13   return maxlen; 
+14   }
 15    
 16};
