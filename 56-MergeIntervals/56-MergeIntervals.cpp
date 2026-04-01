4b82846c82400c6bd6070e980f36d843645b7875
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-04-01 18:20:25 +0530

    File updated at 01/04/2026, 18:20:24

--- 56-MergeIntervals/56-MergeIntervals.cpp
+++ 56-MergeIntervals/56-MergeIntervals.cpp
@@ -1,26 +1,18 @@
-// Last updated: 10/01/2026, 11:52:35
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return {};
-        
-        // Step 1: Sort intervals by start time
-        sort(intervals.begin(), intervals.end());
-        
-        vector<vector<int>> merged;
-        merged.push_back(intervals[0]);
-        
-        // Step 2: Traverse and merge
-        for (int i = 1; i < intervals.size(); i++) {
-            if (merged.back()[1] >= intervals[i][0]) {
-                // Overlap → merge
-                merged.back()[1] = max(merged.back()[1], intervals[i][1]);
-            } else {
-                // No overlap → add new interval
-                merged.push_back(intervals[i]);
-            }
-        }
-        
-        return merged;
-    }
-};
+// Last updated: 01/04/2026, 18:20:24
+1class Solution {
+2public:
+3  
+4    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+5       vector<vector<int>> res;
+6       sort(intervals.begin(),intervals.end());
+7
+8    for (int i = 0; i < intervals.size(); i++) {
+9        if (res.empty() || res.back()[1] < intervals[i][0]) {
+10            res.push_back(intervals[i]);
+11        } else {
+12            res.back()[1] = max(res.back()[1], intervals[i][1]);
+13        }
+14    }
+15    return res;
+16    }
+17};
