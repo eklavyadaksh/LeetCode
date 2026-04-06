7d92901299d754471c8374560a3df08ca6a9df3e
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-04-06 18:59:22 +0530

    File updated at 06/04/2026, 18:59:22

--- 231-PowerofTwo/231-PowerofTwo.cpp
+++ 231-PowerofTwo/231-PowerofTwo.cpp
@@ -1,14 +1,31 @@
-// Last updated: 06/04/2026, 18:24:16
+// Last updated: 06/04/2026, 18:59:22
 1class Solution {
 2public:
-3    bool check(vector<int>& nums) {
-4        int count=0;
-5        if(is_sorted(nums.begin(),nums.end()))
-6        return true;
-7        for(int i=1;i<nums.size();i++){
-8            if(nums[i-1]>nums[i])
-9            count++;
-10            if(nums[nums.size()-1]>nums[0])
-11             count++;
-12        } return count==1;}
-13};
+3    bool isPalindrome(string s) {
+4       
+5       int j = 0;
+6for(int i = 0; i < s.length(); i++){
+7    if(isalnum(s[i])){
+8        s[j++] = s[i];
+9    }
+10}
+11s.resize(j);
+12       for(int i = 0; i < s.length(); i++){
+13    s[i] = tolower(s[i]);
+14}
+15       string v = s; 
+16       reverse(v.begin(), v.end());
+17     
+18       if(v==s )
+19        return true;
+20
+21
+22
+23
+24
+25        return false; }
+26
+27        
+28
+29    
+30};
