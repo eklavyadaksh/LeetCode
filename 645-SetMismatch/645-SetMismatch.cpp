2dfdc066e404a6c447b02c1f7d90e95a6c852d19
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-04-12 14:01:08 +0530

    File updated at 4/12/2026, 2:01:07 PM

--- 645-SetMismatch/645-SetMismatch.cpp
+++ 645-SetMismatch/645-SetMismatch.cpp
@@ -1,18 +1,16 @@
-// Last updated: 4/12/2026, 12:09:27 PM
+// Last updated: 4/12/2026, 2:01:07 PM
 1class Solution {
 2public:
-3    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-4        vector<int> ans;
-5        int count=0;
-6        for(int i=0;i<nums.size();i++){
-7             int max=nums[i];
-8            for(int j=0;j<nums.size();j++){
-9           
-10            if(nums[j]<max)
-11             count++;
-12        }
-13        ans.push_back(count);
-14        count=0;
-15    }
-16   return ans; }
-17};
+3    vector<int> findDisappearedNumbers(vector<int>& nums) {
+4        unordered_map<int,int> mp;
+5            vector<int> ans;
+6            for(auto x:nums)mp[x]++;
+7            for(int i=0;i<nums.size();i++){
+8               
+9                
+10                if (!mp.contains(i+1))
+11                  ans.push_back(i+1);
+12
+13            }return ans;
+14    }
+15};
