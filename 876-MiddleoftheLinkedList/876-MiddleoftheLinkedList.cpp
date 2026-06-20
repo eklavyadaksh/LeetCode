dd9c6fa23d52e60140645967a708f90cd1ea4d59
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-06-20 18:37:49 +0530

    File updated at 6/20/2026, 6:37:49 PM

--- 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.cpp
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.cpp
@@ -1,23 +1,25 @@
-// Last updated: 6/20/2026, 6:34:04 PM
+// Last updated: 6/20/2026, 6:37:49 PM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
 4 *     int val;
 5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12public:
-13    ListNode* middleNode(ListNode* head) {
-14        ListNode* slow=head;
-15        ListNode* fast=head;
-16        while(fast && fast->next){
-17            slow=slow->next;
-18            fast=fast->next->next;
-19        }
-20        return slow;
-21    }
-22};
+6 *     ListNode(int x) : val(x), next(NULL) {}
+7 * };
+8 */
+9class Solution {
+10public:
+11    bool hasCycle(ListNode *head) {
+12        ListNode* slow=head;
+13        ListNode* fast=head;
+14        while(fast && fast->next){
+15            slow=slow->next;
+16            fast=fast->next->next;
+17
+18           if(slow==fast)
+19            return true;
+20        }
+21        
+22        return false;
+23    }
+24};
