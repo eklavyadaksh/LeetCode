e5f6e7a2f9dc1951d5bd0aa329a797bbc6134e5c
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-07-01 12:14:24 +0530

    File updated at 7/1/2026, 12:14:24 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.cpp
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.cpp
@@ -1,22 +1,24 @@
-// Last updated: 7/1/2026, 11:35:59 AM
+// Last updated: 7/1/2026, 12:14:24 PM
 1class Solution {
 2public:
-3    
-4    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-5
-6    unordered_set<int> s(nums1.begin(), nums1.end());
-7
-8    vector<int> ans;
-9
-10    for(int x : nums2){
-11
-12        if(s.count(x)){
-13            ans.push_back(x);
-14            s.erase(x);      // avoid duplicates
-15        }
-16    }
-17
-18    return ans;
-19}
-20};
-21
+3    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+4        int p1=0,p2=0;
+5         vector<int> v;
+6         sort(nums1.begin(),nums1.end());
+7         sort(nums2.begin(),nums2.end());
+8         while(p1<nums1.size() && p2<nums2.size()){
+9            if(nums1[p1]==nums2[p2]){
+10                v.push_back(nums1[p1]);
+11                p1++;
+12                p2++;
+13            }
+14            else if(nums1[p1]>nums2[p2])
+15               p2++;
+16               else
+17                p1++;
+18
+19              
+20         }
+21         return v;
+22    }
+23};
