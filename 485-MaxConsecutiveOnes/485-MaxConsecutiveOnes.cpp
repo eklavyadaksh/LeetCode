59d3f10db151fd903a4b4ac60d27255c2114875a
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-07-21 19:40:11 +0530

    File updated at 7/21/2026, 7:40:10 PM

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
@@ -1,18 +1,19 @@
-// Last updated: 10/01/2026, 11:52:32
-class Solution {
-public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        int maxCount = 0;
-        int count = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                maxCount = max(count, maxCount);
-                count = 0;
-            } else {
-                count++;
-            }
-        }
-        maxCount = max(count, maxCount);
-        return maxCount;
-    }
-};
+// Last updated: 7/21/2026, 7:40:10 PM
+1class Solution {
+2public:
+3    int findMaxConsecutiveOnes(vector<int>& nums) {
+4        int count=0;
+5        int maxcount=0;
+6        for(int i=0;i<nums.size();i++){
+7            if(nums[i]==0){
+8                maxcount=max(maxcount,count);
+9                count=0;
+10            }
+11            else{
+12                count++;
+13            }
+14        }
+15        maxcount=max(maxcount,count);
+16        return maxcount;
+17    }
+18};
