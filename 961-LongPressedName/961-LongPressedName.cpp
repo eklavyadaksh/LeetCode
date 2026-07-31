cceb8b0cec79c6edf3d74d2e59c1200e717622f7
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-07-31 19:23:09 +0530

    Create 961-LongPressedName

---
+++ 961-LongPressedName/961-LongPressedName.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/31/2026, 7:23:09 PM
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+    int i = 0, j = 0;
+
+    while (j < typed.size()) {
+        if (i < name.size() && name[i] == typed[j]) {
+            i++;
+            j++;
+        }
+        else if (j > 0 && typed[j] == typed[j - 1]) {
+            j++;  // long press
+        }
+        else {
+            return false;
+        }
+    }
+
+    return i == name.size();
+}
+};
