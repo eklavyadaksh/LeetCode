ef29f83ad03a5747fc545c6a38177d9d8d3dda4f
Eklavya Daksh <eklavyadaksh4@gmail.com>
2026-07-31 19:22:27 +0530

    Create 2619-CategorizeBoxAccordingToCriteria

---
+++ 2619-CategorizeBoxAccordingToCriteria/2619-CategorizeBoxAccordingToCriteria.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/31/2026, 7:22:26 PM
+class Solution {
+public:
+    string categorizeBox(int length, int width, int height, int mass) {
+
+        long long volume = 1LL * length * width * height;
+
+        bool bulky = (length >= 10000 ||
+                      width  >= 10000 ||
+                      height >= 10000 ||
+                      volume >= 1000000000LL);
+
+        bool heavy = (mass >= 100);
+
+        if (bulky && heavy) return "Both";
+        if (bulky)          return "Bulky";
+        if (heavy)          return "Heavy";
+        return "Neither";
+    }
+};
